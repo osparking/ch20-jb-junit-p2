5d765e5ebfbbee91a9f67fe54da26285a9bd0f28
osparking <jbpark03@gmail.com>
2024-02-10 10:44:20 +0900

    비즈니스 항공편 시험 메소드 수정

--- src/test/java/space/bum/junit/tdd/AirportTest.java
+++ src/test/java/space/bum/junit/tdd/AirportTest.java
@@ -47,5 +47,41 @@ class AirportTest {
       assertEquals(1, economyFlight.getPassengers().size());
     }
   }
+  
+  @Nested
+  @DisplayName("하나의 비즈니스 항공편 시험")
+  class BusinessTest {
+    private Flight businessFlight;
+    
+    @BeforeEach
+    void setup() {
+      businessFlight = new BusinessFlight("2");
+    }
+    
+    @Test
+    void business_일반승객탑승_시험() {
+      Passenger bong = new Passenger("봉", false);
+      
+      assertEquals("2", businessFlight.getId());
+      assertEquals(false, businessFlight.addPassenger(bong));
+      assertEquals(0, businessFlight.getPassengers().size());
+      
+      assertEquals(false, businessFlight.removePassenger(bong));
+      assertEquals(0, businessFlight.getPassengers().size());
+    }
+    
+    @Test
+    void business_vip승객탑승_시험() {
+      Passenger choi = new Passenger("최", true);
+      
+      assertEquals("2", businessFlight.getId());
+      assertEquals(true, businessFlight.addPassenger(choi));
+      assertEquals(1, businessFlight.getPassengers().size());
+      assertEquals("최", businessFlight.getPassengers().get(0).getName());
+      
+      assertEquals(false, businessFlight.removePassenger(choi));
+      assertEquals(1, businessFlight.getPassengers().size());
+    }
+  }
 
 }
